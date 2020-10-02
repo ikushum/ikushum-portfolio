0a2b5c039a769a84ab91c2cd73e6d5907250d977
Ishan Subedi <ikushum@gmail.com>
2020-10-03 01:01:52 +0545

    create About section

--- components/About.vue
+++ components/About.vue
@@ -1,5 +1,40 @@
 <template>
-  <div class="min-full-vh">
-    About
-  </div>
+  <v-container class="circle-bg min-full-vh d-flex align-center text-center">
+    <v-row>
+      <v-col class="d-none d-lg-block" cols="12" lg="6">
+        <v-img contain src="/ishan.png" />
+      </v-col>
+
+      <v-col cols="12" lg="6" class="d-flex justify-center align-start flex-column text-left px-12">
+        <h2 class="display-2">
+          About Me
+        </h2>
+
+        <div class="my-5">
+          <h4 class="headline">
+            Hello I'am <span class="primary--text font-italic"> Ishan Subedi</span>
+          </h4>
+
+          <h4 class="subtitle-1">
+            Vue / Vuetify and Nuxt Developer
+          </h4>
+        </div>
+
+        <p class="my-5 grey--text body-1">
+          I'm a <strong> Freelance Frontend Engineer </strong> based in Kathmandu, Nepal.
+          Have been providing value to <strong> high profile clients </strong> like yourself for 4 years.
+          I've helped my clients build amazing web apps and watch them grow bigger and perform amazingly well
+          in the market
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<style scoped>
+  .circle-bg {
+    background-size: 40%;
+    background-position: center;
+    background-image: url('/circle.svg');
+  }
+</style>
--- components/Header.vue
+++ components/Header.vue
@@ -3,23 +3,25 @@
     gradient="to right top, rgba(0, 0, 0, 0.33), rgba(0, 0, 0, 0.7)"
     src="/header-bg.svg"
     cover
-    class="min-full-vh black d-flex align-center text-center white--text"
+    class="full-vh black d-flex align-center text-center white--text"
   >
-    <h1 class="display-2 font-weight-bold">
-      Vue / Vuetify And Nuxt Rockstar
-    </h1>
+    <v-container>
+      <h1 class="display-2 font-weight-bold">
+        Vue / Vuetify And Nuxt Rockstar
+      </h1>
 
-    <div class="my-10">
-      <h2 class="headline">
-        Namaste, I’m Ishan Subedi
-      </h2>
-      <h3 class="body-1">
-        From the roots of himalaya
-      </h3>
-    </div>
+      <div class="my-10">
+        <h2 class="headline">
+          Namaste, I’m Ishan Subedi
+        </h2>
+        <h3 class="body-1">
+          From the roots of himalaya
+        </h3>
+      </div>
 
-    <v-btn large color="primary" depressed rounded>
-      About Me
-    </v-btn>
+      <v-btn large color="primary" depressed rounded>
+        About Me
+      </v-btn>
+    </v-container>
   </v-img>
 </template>
