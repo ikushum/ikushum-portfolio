7443bb3d35bfc836eba526229a0abac666e16a69
Ishan Subedi <ikushum@gmail.com>
2020-10-02 23:03:51 +0545

    ॐ गणेशाय नमः

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-app>
+    <v-app-bar
+      fixed
+      app
+    >
+      <v-toolbar-title v-text="title" />
+
+      <v-spacer />
+
+      <v-btn
+        icon
+      >
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+
+    <v-footer
+      app
+    >
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'ikushum'
+    }
+  }
+}
+</script>
