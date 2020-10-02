c17241b2b24a27099f8d1f98d883992de6904887
Ishan Subedi <ikushum@gmail.com>
2020-10-03 00:34:11 +0545

    add Header comonent

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-img
+    gradient="to right top, rgba(0, 0, 0, 0.33), rgba(0, 0, 0, 0.7)"
+    src="/header-bg.svg"
+    cover
+    class="min-full-vh black d-flex align-center text-center white--text"
+  >
+    <h1 class="display-2 font-weight-bold">
+      Vue / Vuetify And Nuxt Rockstar
+    </h1>
+
+    <div class="my-10">
+      <h2 class="headline">
+        Namaste, I’m Ishan Subedi
+      </h2>
+      <h3 class="body-1">
+        From the roots of himalaya
+      </h3>
+    </div>
+
+    <v-btn large color="primary" depressed rounded>
+      About Me
+    </v-btn>
+  </v-img>
+</template>
