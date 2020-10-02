78cd8c8014c0c102de1538b90ca1b6924913fc3c
Ishan Subedi <ikushum@gmail.com>
2020-10-03 00:34:30 +0545

    use all components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,36 +1,44 @@
 <template>
   <v-app>
     <v-app-bar
-      fixed
       app
+      flat
+      dark
+      hide-on-scroll
+      class="px-12"
+      height="90px"
+      color="transparent"
     >
-      <v-toolbar-title v-text="title" />
+      <app-bar-content />
+    </v-app-bar>
 
-      <v-spacer />
-
-      <v-btn
-        icon
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+    <v-app-bar
+      dark
+      fixed
+      color="#212427"
+      inverted-scroll
+      class="px-12"
+      scroll-threshold="65"
+    >
+      <app-bar-content />
     </v-app-bar>
 
     <v-main>
-      <v-container>
+      <v-container style="max-width:2300px" class="pa-0">
         <nuxt />
       </v-container>
     </v-main>
 
-    <v-footer
-      app
-    >
+    <v-footer>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import AppBarContent from '@/components/AppBarContent'
 export default {
+  components: { AppBarContent },
   data () {
     return {
       title: 'ikushum'
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
 <template>
-  <div> Index</div>
+  <div>
+    <app-header style="margin-top:-90px" />
+    <about />
+  </div>
 </template>
+
+<script>
+import AppHeader from '@/components/Header'
+import About from '@/components/About'
+export default {
+  components: { AppHeader, About }
+}
+</script>
