ac6b4d4b5811ab6cc681cb9d08552cfa87d24668
Ishan Subedi <ikushum@gmail.com>
2020-10-03 00:34:23 +0545

    add app bar content component

---
+++ components/AppBarContent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div style="width:100%" class="d-flex">
+    <v-img contain max-width="100" src="/logo.svg" />
+
+    <v-spacer />
+
+    <v-btn v-for="menuItem in menuItems" :key="menuItem.text" text class="text-capitalize mx-2">
+      {{ menuItem.text }}
+    </v-btn>
+
+    <!-- <v-btn
+      icon
+    >
+      <v-icon>mdi-menu</v-icon>
+    </v-btn> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      menuItems: [
+        { text: 'Home' },
+        { text: 'About Me' },
+        { text: 'Works' },
+        { text: 'Expertie' },
+        { text: 'Journal' },
+        { text: 'Contact' }
+      ]
+    }
+  }
+}
+</script>
